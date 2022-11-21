ed30866732e579a139b690d9f3fb33be9ebba9c2
Divyansh Singh <sdivyansh59@gmail.com>
2022-11-21 20:51:33 +0530

    Post and Get  Method working

--- banking/app/app.go
+++ banking/app/app.go
@@ -1,17 +1,31 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 	// creating our own multiplexer
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
+
+	// gorilla mux router : 3rd party router  
+	router := mux.NewRouter()
 	// define routes
-	mux.HandleFunc("/", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/", greet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer)
+    // it will take only int as id
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:3000", mux))
+	log.Fatal(http.ListenAndServe("localhost:3000", router))
 }
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,"Post request received")
+}
+
--- banking/go.mod
+++ banking/go.mod
@@ -1,3 +1,5 @@
 module github.com/sdivyansh59/banking
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
