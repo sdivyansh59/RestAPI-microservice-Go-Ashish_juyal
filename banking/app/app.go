989952e1bf2ba5e606b1c4aba90b353c53c39b65
Divyansh Singh <sdivyansh59@gmail.com>
2022-11-21 17:24:26 +0530

    now we define our own multiplexer( not dependent on default multiplexer

--- banking/app/app.go
+++ banking/app/app.go
@@ -6,10 +6,12 @@ import (
 )
 
 func Start() {
+	// creating our own multiplexer
+	mux := http.NewServeMux()
 	// define routes
-	http.HandleFunc("/", greet)
-	http.HandleFunc("/customers", getAllCustomers)
+	mux.HandleFunc("/", greet)
+	mux.HandleFunc("/customers", getAllCustomers)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+	log.Fatal(http.ListenAndServe("localhost:3000", mux))
 }
